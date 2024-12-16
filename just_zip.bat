06f064d50d085a7dcd1ff51d7664acd659f35ef9
Morph21 <38075691+Morph21@users.noreply.github.com>
2024-12-16 18:58:02 +0100

    Added instructions for jar file, changed build scripts

--- just_zip.bat
+++ just_zip.bat
@@ -1,18 +1,5 @@
 echo off
 cls
-echo MercuryTrade build starting
-echo mvn clean package
-call mvn clean package
-echo Copying MercuryTrade.jar from app/target to release_files
-cd app/target
-copy MercuryTrade.jar "../../release_files"
-cd ../..
-echo Launching launch4j.exe to generate MercuryTrade.exe from .jar file
-cd launch4j
-launch4jc.exe ../release_files/release_config.xml
-cd ..
-echo build_mercury_bat completed
-
 echo preparing zip files for release starting...
 echo zipping with jar file start
 cd release_files
@@ -37,6 +24,5 @@ echo zipping with exe completed
 echo zipping with lang started
 echo removing old lang files
 del lang.zip
-call powershell Compress-Archive ../app-shared/src/main/resources/lang lang.zip
+call powershell Compress-Archive ../app-shared/src/main/resources/lang/* lang.zip
 echo zipping with lang ended
-
